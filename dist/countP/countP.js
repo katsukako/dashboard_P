f3649c25e3fee5b475f0276eb120f18e1d1f859a
Katsu <134372186+katsukako@users.noreply.github.com>
2024-05-12 19:03:18 +0900

    My feature (#1)
    * delete
    
    * onlyone radio

--- dist/countP/countP.js
+++ dist/countP/countP.js
@@ -1,11 +1,21 @@
-document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
-    checkbox.addEventListener('change', calculateTotal);
+let lastCheckedRadio = null;
+
+document.querySelectorAll('input[type="radio"]').forEach(radio => {
+    radio.addEventListener('click', () => {
+        if (radio === lastCheckedRadio) {
+            radio.checked = false;
+            lastCheckedRadio = null;
+        } else {
+            lastCheckedRadio = radio;
+        }
+        calculateTotal();
+    });
 });
 
 function calculateTotal() {
     let totalScore = 0;
-    document.querySelectorAll('input[type="checkbox"]:checked').forEach(checkedBox => {
-        totalScore += parseInt(checkedBox.dataset.score, 10);
+    document.querySelectorAll('input[type="radio"]:checked').forEach(checkedRadio => {
+        totalScore += parseInt(checkedRadio.dataset.score, 10);
     });
     document.getElementById('totalScore').textContent = totalScore;
 }
